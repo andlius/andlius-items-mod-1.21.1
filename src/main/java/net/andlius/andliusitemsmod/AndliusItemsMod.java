35108687bebcde40bb2df87529363be10c448e55
Andlius <epic_129@hotmail.com>
2025-09-14 00:22:23 -0500

    rat

---
+++ src/main/java/net/andlius/andliusitemsmod/AndliusItemsMod.java
@@ -0,0 +1,40 @@
+package net.andlius.andliusitemsmod;
+
+import com.mojang.logging.LogUtils;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.config.ModConfig;
+import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
+import net.neoforged.neoforge.event.server.ServerStartingEvent;
+import org.slf4j.Logger;
+
+@Mod(AndliusItemsMod.MOD_ID)
+public class AndliusItemsMod {
+    public static final String MOD_ID = "andliusitemsmod";
+    public static final Logger LOGGER = LogUtils.getLogger();
+
+    public AndliusItemsMod(IEventBus modEventBus, ModContainer modContainer) {
+        modEventBus.addListener(this::commonSetup);
+        modEventBus.addListener(this::addCreative);
+
+        // Register our mod's ModConfigSpec so that FML can create and load the config file for us
+        modContainer.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {
+
+    }
+
+    private void addCreative(BuildCreativeModeTabContentsEvent event) {
+        // Code to add items/blocks to creative tabs
+    }
+
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event) {
+
+    }
+}
